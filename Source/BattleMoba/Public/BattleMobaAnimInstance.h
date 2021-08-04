436a1ab67527fca724fd59e45fc0503c2f69d865
razilah <razisbetterthanyou@gmail.com>
2021-08-04 21:05:49 +0800

    Root Motion Movement Update

--- Source/BattleMoba/Public/BattleMobaAnimInstance.h
+++ Source/BattleMoba/Public/BattleMobaAnimInstance.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+ // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Net/UnrealNetwork.h"
 #include "BattleMobaAnimInstance.generated.h"
 
 /**
@@ -14,4 +15,15 @@ class BATTLEMOBA_API UBattleMobaAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Replicated, BlueprintReadWrite)
+	float Speed = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, Replicated, BlueprintReadWrite)
+	bool bMoving = true;
 };
