e2cc730cf246d8194cd7e3e92f5993696e55e530
Mira <miramuzafar@todak.com>
2021-09-30 14:17:45 +0800

    Update health percentage

--- Source/BattleMoba/Private/BattleMobaCharacter.cpp
+++ Source/BattleMoba/Private/BattleMobaCharacter.cpp
@@ -193,7 +193,7 @@ void ABattleMobaCharacter::OnRep_Health()
 			FString TheFloatStr = FString::SanitizeFloat(this->Health);
 
 			HealthText->SetText(FText::FromString(TheFloatStr));
-			HealthBar->SetPercent(FMath::Clamp(this->Health / 100.0f, 0.0f, 1.0f));
+			HealthBar->SetPercent(FMath::Clamp(this->Health / this->MaxHealth, 0.0f, 1.0f));
 		}
 	}
 	//this->Health = UGestureInputsFunctions::UpdateProgressBarComponent(this->WidgetHUD, "HPBar", "Health", "HP", "Pain Meter", this->Health, this->MaxHealth);
@@ -1222,8 +1222,8 @@ void ABattleMobaCharacter::DoDamage_Implementation(AActor* HitActor)
 
 		/**		Apply Damage */
 		this->ActualDamage = UGameplayStatics::ApplyDamage(HitActor, this->ActualDamage, nullptr, this, nullptr);
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Magenta, FString::Printf(TEXT("Damage Applied: %f"), this->BaseDamage));
-		UE_LOG(LogTemp, Warning, TEXT("Damage Applied: %f"), this->BaseDamage);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Magenta, FString::Printf(TEXT("Damage Applied: %f"), this->ActualDamage));
+		UE_LOG(LogTemp, Warning, TEXT("Damage Applied: %f"), this->ActualDamage);
 
 	}
 }
